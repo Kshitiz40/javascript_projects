2961250681ec5ab420bec0666fd6392d6a1f9335
Kshitiz40 <116447782+Kshitiz40@users.noreply.github.com>
2024-06-08 05:54:51 +0000

    learned variables and their uses

---
+++ basics/variables.js
@@ -0,0 +1,18 @@
+if(1==1)
+{
+    const studentId = 12342
+    let studentName = "kshitiz"
+    var studentEmail = 'kshitiz@gmail.com'
+    studentCity = 'delhi'
+    let studentVariable;
+
+    console.table([studentId,studentName,studentEmail,studentCity,studentVariable])
+}
+if(1==1)
+    {
+        var studentEmail = 'kshit@gmailcom' //scope problem- functional and block scope
+        console.log(studentEmail)
+        let studentName = 'rakesh'
+        console.log(studentName)
+        //let studentName = 'ghanakash'
+    }
