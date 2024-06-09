76e4071c91b4a874d98c806fa4dfcc7db0f1a544
Kshitiz40 <116447782+Kshitiz40@users.noreply.github.com>
2024-06-09 11:15:22 +0000

    new commit

---
+++ string.js
@@ -0,0 +1,6 @@
+let name = "kshitiz"
+const salary = 10000
+
+console.log(`Hello! my name is ${name.toUpperCase()} and my salary is ${salary}`)
+let gameName = new String('RAGE.SOul')
+console.log(name)
