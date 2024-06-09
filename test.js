697d637f203c8a983403015fec90ee89ba2b365c
Kshitiz40 <116447782+Kshitiz40@users.noreply.github.com>
2024-06-09 10:01:46 +0000

    till 10

--- test.js
+++ test.js
@@ -1,1 +1,64 @@
-console.log("hello")
+// let score = true
+
+// console.log(typeof(score))
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+// let isLoggedIn =1
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// let value =3
+// let negValue = -value
+// //console.log(negValue)
+// let str1 = "hello"
+// let str2 = " world"
+// let str3 = str1+str2
+//console.log(str3)
+//console.log(1+"2")
+//console.log("1"+2+2)
+//console.log(1+2+"3")
+// console.log(true)
+// console.log(+true)
+// console.log(+"")
+
+// let num1,num2,num3;
+// num1 = num2 = num3 = 2+2
+
+// console.log("2">1)
+// console.log("02">1)
+// console.log(2>"1")
+// console.log(2>"01")
+
+// console.log(null >0)
+// console.log(null == 0)
+// console.log(null>=0)
+
+//Primitive(call by value by default)
+//7 types - String, Number, Boolean, null, Undefined, Symbol, BigInt
+
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+
+// console.log(id===anotherId)
+
+// const bigNum = 123456789123456789n
+
+//Reference (Non primitive)
+//Arrays, Objects, Functions
+
+// const fruits = ["mango","banana"]
+// let obj = {
+//     name: "kshitiz",
+//     age: 20,
+// }
+// const myFunction = function(age) {
+//     console.log("Hello world" + age)
+// }
+// myFunction(10)
+
+//stack(primitive) and heap(non primitive) memory
+
+let myName = "kshitiz"
