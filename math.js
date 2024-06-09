2da65294b5cfa77972f07f30f34f40c20e75dbed
Kshitiz40 <116447782+Kshitiz40@users.noreply.github.com>
2024-06-09 11:40:53 +0000

    math-important

--- math.js
+++ math.js
@@ -1,1 +1,25 @@
-const
+// const score = 400
+// console.log(score)
+
+// const balance = new Number(100.34345)
+// console.log(balance)
+
+// console.log(typeof balance.toString())
+// console.log(balance.toFixed(2))
+
+//  const otherNumber = 123.7676
+
+//  console.log(typeof otherNumber.toPrecision(3))//returns a string
+
+//  const hunderd = 1000000
+//  console.log(hunderd.toLocaleString('en-UK'))
+
+//  console.log(Math)
+
+ console.log(Math.random().toFixed(2))//between 0 and 1
+
+ console.log(Math.round((Math.random()*10)+1));
+ const min =10
+ const max=20
+
+console.log(Math.floor(Math.random()*(max-min +1) +min))
